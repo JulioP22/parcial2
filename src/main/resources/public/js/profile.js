99b22b7b690b8672e835dbb410869f5d831fec49
Bryan Rodríguez <bryanrd05@gmail.com>
2018-07-18 10:30:27 -0400

    Galeria de fotos inicial

--- src/main/resources/public/js/profile.js
+++ src/main/resources/public/js/profile.js
@@ -41,3 +41,31 @@ $(function () {
         }
     });
 });
+
+function goToGallery(){
+    $("#wall_normal").hide();
+    $("#wall_gallery").show();
+}
+
+function backToWall(){
+    $("#wall_gallery").hide();
+    $("#wall_normal").show();
+}
+
+// portfolio
+$('.gallery ul li a').click(function() {
+    var itemID = $(this).attr('href');
+    $('.gallery ul').addClass('item_open');
+    $(itemID).addClass('item_open');
+    return false;
+});
+$('.close').click(function() {
+    $('.port, .gallery ul').removeClass('item_open');
+    return false;
+});
+
+$(".gallery ul li a").click(function() {
+    $('html, body').animate({
+        scrollTop: parseInt($("#top").offset().top)
+    }, 400);
+});
